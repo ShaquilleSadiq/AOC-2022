fd04a0baea3309875b179cdc333f35e1b1357e8c
Shaquille Sadiq <ayobamissadiq@gmail.com>
2022-12-10 00:47:16 -0500

    Day_10

--- Day_10/index.js
+++ Day_10/index.js
@@ -1,16 +1,44 @@
 const fs = require("fs");
 const path = require("path");
 
+let cyc = 0;
+let x = 1;
+let strength = 0;
+
 const main = async () => {
     const lines = readFile("input.txt");
+    const funcs = { noop, addx };
 
+    console.log("P2: ");
     for (let line of lines) {
-        console.log(line);
+        const [instruct, num] = line.split(" ");
+
+        funcs[instruct](num);
     }
 
-    console.log("Nothing here");
+    console.log("\nP1: ", strength);
 };
 
+const addx = (num) => {
+    runCycle();
+    runCycle();
+    x += parseInt(num);
+};
+
+const noop = () => runCycle();
+
+const runCycle = () => {
+    cyc++;
+    const diff = Math.abs(x - ((cyc % 40) - 1));
+    const symbol = diff == 0 || diff == 1 ? "█" : ".";
+    process.stdout.write(symbol);
+
+    // new line
+    if (cyc % 40 == 0) console.log();
+
+    // calulate strength
+    if ((cyc - 20) % 40 == 0) strength += cyc * x;
+};
 
 const readFile = (filename) => fs.readFileSync(path.join(__dirname, filename), "utf8").split("\n");
 
